e17bdac407f9375b73b2a248bbbc6195d269d9ef
fanhailong <fanhailong@hellogeek.com>
2019-11-18 19:28:19 +0800

    添加测试环境域名

--- jksdk/src/main/java/com/comm/jksdk/http/Api.java
+++ jksdk/src/main/java/com/comm/jksdk/http/Api.java
@@ -19,7 +19,7 @@ public interface Api {
 
     class URL_TEST{//测试环境
 //        public static final String APP_WEATHER_DOMAIN = "http://172.16.11.247:8974/adsenseapi/"; //测试环境
-        public static final String APP_WEATHER_DOMAIN = "http://testadsenseapi.hellogeek.com"; //测试环境
+        public static final String APP_WEATHER_DOMAIN = "http://testadsenseapi.hellogeek.com/adsenseapi/"; //测试环境
 
     }
 
